a048142702e793ce2e73fe286c2a958b49a2f82a
KetsiaAnnaelle <158278455+KetsiaAnnaelle@users.noreply.github.com>
2025-09-01 05:01:26 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-84$$!%nsnn_8q^vss-+6!8fsgbi)04y8hvds9(5%8h#2@@-d+p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["test-aquand-1.onrender.com","localhost", "127.0.0.1"]
 
 
 # Application definition
